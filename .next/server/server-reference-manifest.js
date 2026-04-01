451f1e2c242e4e68328baa48c979149acfb8782a
praveen-sharma-t <praveen.s@turing.com>
2026-04-01 22:42:51 +0530

    fix: next version updated

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Pw09cONGyP8rV+DHcl/fknkf72bCysMuNJ5q4E++8Fc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
